d0bd7d983df35f9aae42511abd09655f4164afc4
Vlad <m201400@gmai.com>
2024-03-19 18:47:12 +0200

    Loading tl

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,7 +1,16 @@
-export default function Loading() {
+import { getTranslations } from "next-intl/server"
+
+type LoadingProps = {
+  params: {
+    locale: Locale
+  }
+}
+
+export default async function Loading({ params }: LoadingProps) {
+  const tl = await getTranslations("States")
   return (
     <main className="bg-bgPrimary grid place-content-center min-h-[50vh]">
-      <h1 className="fluid-4xl text-textPrimary font-bold">Loading...</h1>
+      <h1 className="fluid-4xl text-textPrimary font-bold">{tl("Loading")}</h1>
     </main>
   )
 }
