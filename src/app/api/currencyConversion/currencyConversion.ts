742c4c38c4174cd7db64d1bb10066867919d38ef
M201200 <m201400@gmail.com>
2025-05-03 16:05:58 +0300

    add more lenient date check in getCurrencyConversion

--- src/app/api/currencyConversion/currencyConversion.ts
+++ src/app/api/currencyConversion/currencyConversion.ts
@@ -19,39 +19,42 @@ type ExchangeRates = {
 }
 
 export default async function getCurrencyConversion() {
-  const currentDate = new Date(Date.now()).toISOString().slice(0, 9)
+  const currentYearMonth = new Date(Date.now()).toISOString().slice(0, 7) // YYYY-MM
 
   const exchangeRatesUSDArr = await db
     .select()
     .from(exchange_rates_USD)
-    .where(like(exchange_rates_USD.date, `${currentDate}%`))
+    .where(like(exchange_rates_USD.date, `${currentYearMonth}%`))
 
   const exchangeRatesUSD = exchangeRatesUSDArr[0]
   if (!exchangeRatesUSD) {
-    const response = await fetch(
-      `https://api.currencyapi.com/v3/latest?apikey=${process.env.CURRENCY_API_KEY}&base_currency=USD&currencies=EUR%2CMDL`,
-      {
-        method: "GET",
-      }
-    )
-    const result = (await response.json()) as ExchangeRates
+    try {
+      const response = await fetch(
+        `https://api.currencyapi.com/v3/latest?apikey=${process.env.CURRENCY_API_KEY}&base_currency=USD&currencies=EUR%2CMDL`,
+        {
+          method: "GET",
+        }
+      )
+      const result = (await response.json()) as ExchangeRates
 
-    await db
-      .insert(exchange_rates_USD)
-      .values({
-        id: 1,
+      await db
+        .insert(exchange_rates_USD)
+        .values({
+          id: 1,
+          EUR: result.data.EUR.value,
+          MDL: result.data.MDL.value,
+          date: result.meta.last_updated_at.slice(0, 10),
+        })
+        .onConflictDoNothing()
+        .execute()
+
+      return {
         EUR: result.data.EUR.value,
         MDL: result.data.MDL.value,
-        date: result.meta.last_updated_at.slice(0, 10),
-      })
-      .onConflictDoNothing()
-      .execute()
-
-    return {
-      EUR: result.data.EUR.value,
-      MDL: result.data.MDL.value,
-    } as Rates
-  } else {
+      } as Rates
+    } catch {
+      console.error("Error fetching and updating exchange rates")
+    }
   }
   return {
     EUR: exchangeRatesUSD.EUR,
